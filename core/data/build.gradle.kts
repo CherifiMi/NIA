071f1c7539059d10070139ac13c6a8c6deb8760c
CherifiMi <cherifi154949@gmail.com>
2023-06-18 21:22:06 +0100

    .

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,44 +1,34 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("nowinandroid.android.library")
+    id("nowinandroid.android.library.jacoco")
+    id("nowinandroid.android.hilt")
+    id("kotlinx-serialization")
 }
 
 android {
     namespace = "com.example.data"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //implementation(project(":core:analytics"))
+    implementation(project(":core:common"))
+    //implementation(project(":core:database"))
+    //implementation(project(":core:datastore"))
+    //implementation(project(":core:model"))
+    //implementation(project(":core:network"))
+    //implementation(project(":core:notifications"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.json)
+
+    //testImplementation(project(":core:datastore-test"))
+    testImplementation(project(":core:testing"))
 }
